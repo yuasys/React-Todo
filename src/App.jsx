1b62753c04704a4e1486a1c116d93daadb6cf85b
yuasys <yuasys@gmail.com>
2022-10-22 12:44:18 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            あああああ
+            <div>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            いいいいい
+            <div>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>
+            ううううう
+            <div>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
