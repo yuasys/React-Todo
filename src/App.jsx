1bf5c214655985df1afec5cd49f9764096f6a930
yuasys <yuasys@gmail.com>
2022-10-22 13:06:50 +0000

    Update App.jsx and styles.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,35 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
           <li>
-            あああああ
-            <div>
+            <div className="list-row">
+              あああああ
               <button>完了</button>
               <button>削除</button>
             </div>
           </li>
           <li>
-            いいいいい
-            <div>
+            <div className="list-row">
+              いいいいい
               <button>完了</button>
               <button>削除</button>
             </div>
           </li>
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
         <ul>
           <li>
-            ううううう
-            <div>
+            <div className="list-row">
+              ううううう
               <button>戻す</button>
             </div>
           </li>
