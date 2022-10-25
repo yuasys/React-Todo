348ae57c2f8e036818a5c2ced7557251c365fa30
yuasys <yuasys@gmail.com>
2022-10-25 11:44:19 +0000

    Update 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,15 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるtodo最大個までだよ~。消化しろ~！
+        </p>
+      )}
+
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
